8125937b0faf23244bc2f15aa5356a7192181b9b
Kruthik Ballari <112860541+KruthikB@users.noreply.github.com>
2024-11-08 10:46:50 +0530

    Add files via upload
    Kindly install all the required libraries and modules since I am unable to upload the venv file here

---
+++ Mini Project/config.py
@@ -0,0 +1,16 @@
+import os
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
+
+    #Database for SQLite
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'
+
+    #Database for Postgres
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:kruthik1207@localhost:5432/users'
+    
+    #Database for MySQL
+
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Kruth!k1207@localhost/users'
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
